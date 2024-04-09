08ebe70ee62aeef595405c7f47a2348c1c8cc86d
thoroc <thomas.a.roche@gmail.com>
2024-04-09 11:23:34 +0100

    setting up linting/formatting

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,28 +1,35 @@
-import { awscdk } from 'projen';
+import { awscdk } from "projen";
+import { TrailingComma } from "projen/lib/javascript";
 
 const dependencies = [
-  'constructs',
-  'openapi-types',
-  'ts-json-schema-generator',
-  'aws-cdk-lib',
+  "constructs",
+  "openapi-types",
+  "ts-json-schema-generator",
+  "aws-cdk-lib",
 ];
 
 const project = new awscdk.AwsCdkConstructLibrary({
-  name: 'aws-construct-openapi',
-  author: 'thoroc',
-  authorAddress: 'thomas.a.roche@gmail.com',
-  authorName: 'Thomas Roche',
-  defaultReleaseBranch: 'main',
-  keywords: ['aws', 'cdk', 'openapi', 'api', 'gateway', 'rest'],
-  repositoryUrl: 'https://github.com/thoroc/aws-construct-openapi.git',
-  description: 'AWS CDK Construct for OpenAPI',
+  name: "aws-construct-openapi",
+  author: "thoroc",
+  authorAddress: "thomas.a.roche@gmail.com",
+  authorName: "Thomas Roche",
+  defaultReleaseBranch: "main",
+  keywords: ["aws", "cdk", "openapi", "api", "gateway", "rest"],
+  repositoryUrl: "https://github.com/thoroc/aws-construct-openapi.git",
+  description: "AWS CDK Construct for OpenAPI",
 
   projenrcTs: true,
-  jsiiVersion: '~5.0.0',
-  cdkVersion: '2.1.0',
+  jsiiVersion: "~5.0.0",
+  cdkVersion: "2.1.0",
+  jest: true,
+  eslint: true,
+  prettier: true,
+  prettierOptions: {
+    settings: { singleQuote: true, trailingComma: TrailingComma.ALL },
+  },
   deps: dependencies,
   peerDeps: dependencies,
-  devDeps: ['aws-cdk-lib'],
+  devDeps: ["aws-cdk-lib"],
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
