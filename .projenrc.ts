7406770055a43b7bfc8823f69ce2dfe15c94e744
thoroc <thomas.a.roche@gmail.com>
2024-04-09 10:52:28 +0100

    setting up the dependencies correctly

--- .projenrc.ts
+++ .projenrc.ts
@@ -7,13 +7,16 @@ const project = new awscdk.AwsCdkConstructLibrary({
   author: 'thoroc',
   authorAddress: 'thomas.a.roche@gmail.com',
   authorName: 'Thomas Roche',
-  cdkVersion: '2.1.0',
   defaultReleaseBranch: 'main',
-  jsiiVersion: '~5.0.0',
-  projenrcTs: true,
+  keywords: ['aws', 'cdk', 'openapi', 'api', 'gateway', 'rest'],
   repositoryUrl: 'https://github.com/thoroc/aws-construct-openapi.git',
+  description: 'AWS CDK Construct for OpenAPI',
 
+  projenrcTs: true,
+  jsiiVersion: '~5.0.0',
+  cdkVersion: '2.1.0',
   deps: dependencies,
+  peerDeps: dependencies,
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
