589882c8c9cda33c7568ca24cce5aa10e48f533c
thoroc <thomas.a.roche@gmail.com>
2024-04-09 10:45:53 +0100

    added deps

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,15 +1,19 @@
 import { awscdk } from 'projen';
+
+const dependencies = ['constructs', 'openapi-types', 'aws-cdk-lib'];
+
 const project = new awscdk.AwsCdkConstructLibrary({
+  name: 'aws-construct-openapi',
   author: 'thoroc',
   authorAddress: 'thomas.a.roche@gmail.com',
+  authorName: 'Thomas Roche',
   cdkVersion: '2.1.0',
   defaultReleaseBranch: 'main',
   jsiiVersion: '~5.0.0',
-  name: 'aws-construct-openapi',
   projenrcTs: true,
-  repositoryUrl: 'https://github.com/thomas.a.roche/aws-construct-openapi.git',
+  repositoryUrl: 'https://github.com/thoroc/aws-construct-openapi.git',
 
-  // deps: [],                /* Runtime dependencies of this module. */
+  deps: dependencies,
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
