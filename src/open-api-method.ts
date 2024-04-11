f3f236104b59b88e55ee9e47fda8f022d76c0673
thoroc <thomas.a.roche@gmail.com>
2024-04-11 23:31:54 +0100

    fix: JSII finding on method

--- src/open-api-method.ts
+++ src/open-api-method.ts
@@ -1,19 +1,27 @@
 import { OpenApiRequestBody } from './open-api-request-body';
 
+export interface OpenApiMethodParameterSchemaProps {
+  readonly type: string;
+}
+
+export interface OpenApiMethodParameterProps {
+  readonly name: string;
+  readonly in: string;
+  readonly required: boolean;
+  readonly schema: OpenApiMethodParameterSchemaProps;
+}
+
+export interface OpenApiMethodResponseProps {
+  readonly description: string;
+  readonly headers: Record<string, unknown>;
+  readonly content: Record<string, unknown>;
+}
+
 export interface OpenApiMethodProps {
-  readonly parameters: {
-    name: string;
-    in: string;
-    required: boolean;
-    schema: { type: string };
-  }[];
+  readonly parameters: OpenApiMethodParameterProps[];
   readonly requestBody?: OpenApiRequestBody;
   readonly responses: {
-    [key: string]: {
-      description: string;
-      headers: Record<string, unknown>;
-      content: Record<string, unknown>;
-    };
+    [key: string]: OpenApiMethodResponseProps;
   };
   readonly security: { [key: string]: any[] }[];
 }
