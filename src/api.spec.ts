7d0bcfd917ded65a75a54f63519421f9340c4eed
thoroc <thomas.a.roche@gmail.com>
2024-04-11 22:03:52 +0100

    chores: fixed import for fs and path

--- src/api.spec.ts
+++ src/api.spec.ts
@@ -1,5 +1,5 @@
-import fs from 'fs';
-import path from 'path';
+import * as fs from 'fs';
+import * as path from 'path';
 import { App, Stack } from 'aws-cdk-lib';
 import { Model, RequestValidator } from 'aws-cdk-lib/aws-apigateway';
 import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
